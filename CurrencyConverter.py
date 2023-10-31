5e4a5e7c5ec719e1f1af71d51f6a89618b2213a0
DeshiHacker <manis.programmer@gmail.com>
2023-10-31 05:59:49 -0400

    Add files via upload

---
+++ CurrencyConverter.py
@@ -0,0 +1,8 @@
+USD_Rate = float(input("Please input current dollar rate against BDT: "))
+TotalUSD = int(input("How much dollar you want to convert in taka: "))
+
+Calculation = USD_Rate * TotalUSD
+
+print(f"{Calculation} Taka.")
+print("#Deshi hacker#")
+print("manis.programmer@gmail.com")
